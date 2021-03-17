2c565998a7cf4a0febd1cd8250ec93e3d1f6f9fd
Jitender Badoni <jitender.badoni>
2021-03-17 19:42:08 +0530

    Get Quote Screen

--- src/containers/PricingRequest/index.js
+++ src/containers/PricingRequest/index.js
@@ -1,39 +1,34 @@
 import React, {useContext} from 'react';
-import { View, Text } from 'react-native';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    TextInput
+    } from 'react-native';
 import {GradientButton} from '../../components';
-import {logout, RfW} from '../../utils/helpers';
+import {logout, RfH, RfW} from '../../utils/helpers';
 import UserContext from '../Login/user.context';
 import {useNavigation} from '@react-navigation/native';
 import Styles from "./styles";
 import Fonts from "../../theme/Fonts";
 import Colors from "../../theme/Colors";
-import { TextInput } from 'react-native';
-import { TouchableOpacity } from 'react-native';
-import { Picker } from "@react-native-community/picker";
 import  DropDownPicker from "react-native-dropdown-picker";
-
-
-const { Item } = Picker;
+import FAIcon from "react-native-vector-icons/FontAwesome";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 function PricingRequest() {
   const { user, setLogin } = useContext(UserContext);
   const navigation = useNavigation();
 
-  const handleLogout = () => {
-    logout();
-    setLogin(false);
-  };
-
-
   return (
-    <View style={{flex:1, paddingHorizontal:RfW(20), justifyContent:'center'}}>
-      <View style={{alignItems:'center', marginBottom: 20}}>
-          <Text style={{fontFamily: Fonts.bold, fontSize: 20}}>Get Quote</Text>
-      </View>
-      <View>
+      <KeyboardAwareScrollView style={{flex:1, paddingHorizontal:RfW(20)}}>
+            <View style={Styles.headerContainer}>
+                <Text style={Styles.headerText}>Get Quote</Text>
+            </View>
             <View style={{marginTop: 15}}>
               <Text style={Styles.inputLabelText}>Please choose a sub category</Text>
               <DropDownPicker
+                showArrow={true}
                 items={[
                     {label: 'USA', value: 'usa', hidden: true},
                     {label: 'Select one', value: '0'},
@@ -49,44 +44,52 @@ function PricingRequest() {
                 dropDownStyle={{backgroundColor: '#fafafa'}}
                 onChangeItem={item => console.log(item)}
               />
-          </View>
-          <View style={{marginTop: 15, height: 60, width: '100%'}}>
-            <Text style={Styles.inputLabelText}>Volume</Text>
-            <View style={{flex: 1, flexDirection:'row', justifyContent:'space-between'}}>
-              <TextInput placeholder={'Enter location'} style={[Styles.inputText, { flex: 3, height: 45, marginRight: 15}]}/>
-              <DropDownPicker
-                items={[
-                    {label: 'USA', value: 'usa', hidden: true},
-                    {label: 'Units', value: '0'},
-                    {label: 'France', value: 'france'},
-                ]}
-                defaultValue={"0"}
-                globalTextStyle={Styles.dropDownText}
-                containerStyle={{height: 45, flex: 2}}
-                style={{backgroundColor: '#e4e4e4'}}
-                itemStyle={{
-                    justifyContent: 'flex-start',
-                }}
-                dropDownStyle={{backgroundColor: '#fafafa'}}
-                onChangeItem={item => console.log(item)}
-              />
             </View>
+            <View style={Styles.viewVolume}>
+              <Text style={Styles.inputLabelText}>Volume</Text>
+              <View style={Styles.viewVolumeInputContainer}>
+                <TextInput placeholder={'Enter location'} style={[Styles.inputText, { flex: 3, height: 45, marginRight: 15}]}/>
+                <DropDownPicker
+                  items={[
+                      {label: 'USA', value: 'usa', hidden: true},
+                      {label: 'Units', value: '0'},
+                      {label: 'France', value: 'france'},
+                  ]}
+                  defaultValue={"0"}
+                  globalTextStyle={Styles.dropDownText}
+                  containerStyle={{height: 45, flex: 2}}
+                  style={{backgroundColor: '#e4e4e4'}}
+                  itemStyle={{
+                      justifyContent: 'flex-start',
+                  }}
+                  dropDownStyle={{backgroundColor: '#fafafa'}}
+                  onChangeItem={item => console.log(item)}
+                />
+              </View>
+            </View>
+            <View style={{marginTop: 15}}>
+              <Text style={Styles.inputLabelText}>Add Location</Text>
+              <TextInput placeholder={'Enter location'} style={Styles.inputText}/>
+            </View>
+            <View style={{marginTop: 15, height: 60}}>
+              <Text style={Styles.inputLabelText}>Add Picture</Text>
+              <View style={{flex:1, flexDirection:'row', justifyContent:'space-between'}}>
+                <TouchableOpacity style={Styles.iconButton}>
+                  <FAIcon name={"camera"} size={25} style={{marginRight: 10}} color={Colors.grayThree}/>
+                  <Text style={{ color:Colors.grayThree, fontFamily: Fonts.regular}}>Camera</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={Styles.iconButton}>
+                  <FAIcon name={"image"} size={25} style={{marginRight: 10}} color={Colors.grayThree}/>
+                  <Text style={{ color:Colors.grayThree, fontFamily: Fonts.regular}}>Library</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={{marginTop: RfH(10)}}>
+              <TouchableOpacity style={Styles.confirmButton} onPress={() => { }}>
+                  <Text style={Styles.confirmBtnText}>CONFIRM</Text>
+              </TouchableOpacity>
           </View>
-          <View style={{marginTop: 15}}>
-            <Text style={Styles.inputLabelText}>Add Location</Text>
-            <TextInput placeholder={'Enter location'} style={Styles.inputText}/>
-          </View>
-          <View style={{marginTop: 15}}>
-            <Text style={Styles.inputLabelText}>Add Picture</Text>
-            <TextInput placeholder={'Enter location'} style={Styles.inputText}/>
-          </View>
-      </View>
-      <View>
-          <TouchableOpacity style={{ borderRadius: 10, backgroundColor:Colors.mango, paddingVertical: 15, alignItems:'center'}} onPress={() => { }}>
-              <Text style={{fontFamily: Fonts.bold, color: Colors.white}}>CONFIRM</Text>
-          </TouchableOpacity>
-      </View>
-    </View>
+    </KeyboardAwareScrollView>
   );
 }
 
--- src/containers/PricingRequest/styles.js
+++ src/containers/PricingRequest/styles.js
@@ -6,6 +6,47 @@ import Fonts from "../../theme/Fonts";
 import { Colors } from '../../theme';
 
 const styles = StyleSheet.create({
+
+  headerContainer:{
+    alignItems:'center',
+    marginBottom: 20,
+    paddingTop: RfH(70)
+  },
+  confirmButton:{
+    marginTop:20,
+    borderRadius: 10,
+    backgroundColor:Colors.mango,
+    paddingVertical: 15,
+    alignItems:'center'
+  },
+  confirmBtnText:{
+    fontSize: 18,
+    fontFamily:
+    Fonts.regular,
+    color: Colors.white
+  },
+  iconButton:{
+    borderRadius: 10,
+    paddingHorizontal: 45,
+    paddingVertical: 25,
+    backgroundColor:'#ccc',
+    flexDirection:'row',
+    alignItems:'center'
+  },
+  headerText:{
+    fontFamily: Fonts.bold,
+    fontSize: 20
+  },
+  viewVolume:{
+    marginTop: 15,
+    height: 60,
+    width: '100%'
+  },
+  viewVolumeInputContainer:{
+    flex: 1,
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
   listHeaderContainer: {
     paddingHorizontal: RfW(22),
     paddingVertical: RfH(20)
@@ -26,54 +67,6 @@ const styles = StyleSheet.create({
     Fonts.regular,
     fontSize: 16
   },
-  headerText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingTop: RfH(18),
-    paddingBottom: RfH(10),
-    paddingHorizontal: RfW(22)
-  },
-
-  gradientShadow: {
-    shadowOffset: { width: 2, height: 3 },
-    shadowOpacity: 1,
-    shadowColor: 'rgba(255,255,255,0.75)',
-    shadowRadius: RfH(20),
-    backgroundColor: Colors.white,
-    elevation: 5,
-    borderRadius: RfH(20),
-  },
-
-  gradientView: {   
-    paddingHorizontal: RfW(22),
-    paddingTop: RfH(22),
-    paddingBottom: RfH(26),
-    borderRadius: RfH(20),
-  },
-
-  footerView: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: RfW(22),
-    paddingVertical: RfH(22),
-    backgroundColor: Colors.white,
-    marginBottom: RfH(100)
-  },
-
-  viewLine: {
-    flex: 1,
-    flexDirection: 'row',
-    height: RfH(1),
-    backgroundColor: Colors.grayBorder
-  },
-  listItemInnerView: {
-    paddingVertical: RfH(20),
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  }
 });
 
 export default styles;
