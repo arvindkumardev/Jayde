c18a1bd04c7ce20cf3b9804544f2f896330e55c7
Arun Meena <arun.meena@gmail.com>
2021-03-26 12:05:19 +0530

    adding lint and prettier config

--- src/theme/Styles/container.js
+++ src/theme/Styles/container.js
@@ -1,47 +1,47 @@
-import { StyleSheet } from "react-native";
-import Colors from "../Colors";
-import Fonts from "../Fonts";
+import { StyleSheet } from 'react-native';
+import Colors from '../Colors';
+import Fonts from '../Fonts';
 
 const container = StyleSheet.create({
-    btnPrimary:{
-        backgroundColor: Colors.mango
-    },
-    btnSecandary:{
-        backgroundColor: Colors.grayBackground
-    },
-    br10:{
-        borderRadius: 10
-    },
-    w100:{
-        width: '100%'
-    },
-    w45:{
-        width: '45%'
-    },
-    primaryColor:{
-        color: Colors.mango
-    },
-    flexRowSpaceBetween:{
-        flexDirection:'row',
-        justifyContent:'space-between'
-    },
-    alignCenter:{
-        alignItems:'center'
-    },
-    topBorderGray:{
-        paddingTop: 20,
-        borderTopColor: '#ccc',
-        borderTopWidth: 1,
-    },
-    bottomBorderGray:{
-        borderBottomColor: '#ccc',
-        borderBottomWidth: 1,
-        paddingBottom: 20,
-    },
-    flex1SpaceBetween:{
-        flex: 1,
-        justifyContent:'space-between'
-    },
-})
+  btnPrimary: {
+    backgroundColor: Colors.mango,
+  },
+  btnSecandary: {
+    backgroundColor: Colors.grayBackground,
+  },
+  br10: {
+    borderRadius: 10,
+  },
+  w100: {
+    width: '100%',
+  },
+  w45: {
+    width: '45%',
+  },
+  primaryColor: {
+    color: Colors.mango,
+  },
+  flexRowSpaceBetween: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  alignCenter: {
+    alignItems: 'center',
+  },
+  topBorderGray: {
+    paddingTop: 20,
+    borderTopColor: '#ccc',
+    borderTopWidth: 1,
+  },
+  bottomBorderGray: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    paddingBottom: 20,
+  },
+  flex1SpaceBetween: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+});
 
 export default container;
