ba986847b0d6bbd62b90f4485f66822813a3a955
Arvind <arvind.kumar@rhatechnology.com>
2021-06-18 13:01:10 +0530

    Epr add in seller

--- src/containers/Seller/AddressConfirm/index.js
+++ src/containers/Seller/AddressConfirm/index.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable global-require */
 import React, { useLayoutEffect, useContext, useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, Modal, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, Modal, ScrollView, TextInput, } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
 import style from '../../../theme/Styles/container';
@@ -17,15 +17,20 @@ import UserContext from '../../../appContainer/context/user.context';
 import { getSaveData, formatDisplayDate } from '../../../utils/helpers';
 import { LOCAL_STORAGE_DATA_KEY } from '../../../utils/constants';
 import PickupDate from '../modalTimeSlot'
+import DropDown from '../../../components/Picker/index';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
 
 const AddressConfirm = () => {
   const navigation = useNavigation();
   const route = useRoute();
+  const [clickLogin, setClickLogin] = useState(false);
   const { setLoader } = useContext(UserContext);
 
   const [addressData, setAddress] = useState('')
   const [userName, setUserName] = useState('')
   const [userPhone, setUserPhone] = useState('')
+  const [EPRAggregatorName, setEPRAggregatorName] = useState([])
 
   const [timeSlot, setTimeSlot] = useState('')
   const [btnConfirm, setBtnConfirm] = useState(false)
@@ -37,6 +42,13 @@ const AddressConfirm = () => {
 
   const [modalVisible, setModalVisible] = useState(false)
 
+
+  // useEffect(() => {
+  //   if (aggregatorsData) {
+  //     const pickerData = aggregatorsData.map((item) => ({ label: item.name, value: item.id }));
+  //     setEPRAggregatorName(pickerData);
+  //   }
+  // }, [aggregatorsData]);
 
   useEffect(() => {
     if (error)
@@ -90,6 +102,28 @@ const AddressConfirm = () => {
     console.log(item)
     setAddress(item)
   }
+
+  const validationSchema = Yup.object().shape({
+    eprAggregatorId: Yup.string().required('Please Select EPR Aggregators'),
+  });
+
+  const businessForm = useFormik({
+    validateOnChange: true,
+    validateOnBlur: true,
+    initialValues: {
+      eprAggregatorId: '',
+    },
+    validationSchema,
+    onSubmit: () =>
+    _addSchedule(
+        businessForm.values.eprAggregatorId,
+      ),
+  });
+
+  const handlePickupConfirm = async () => {
+    setClickLogin(true);
+    await businessForm.submitForm();
+  };
 
   const _addSchedule = async () => {
     let param = {
@@ -238,6 +272,28 @@ const AddressConfirm = () => {
               </View>
             </View>
           }
+
+          <View style={[AppStyles.mt20]}>
+            <Text style={[AppStyles.txtBlackRegular, AppStyles.f15, AppStyles.mb6, AppStyles.ml5]}>Preferred EPR Partner: Raj</Text>
+          </View>
+
+        <View style={[AppStyles.mt20]}>
+            <Text style={[AppStyles.txtBlackRegular, AppStyles.mb10, AppStyles.ml5, AppStyles.f15]}>EPR Aggregators</Text>
+            <DropDown
+              placeholderText="Select one"
+              items={EPRAggregatorName}
+              itemStyle={{ color: '#000' }}
+              onValueChange={(id) => businessForm.setFieldValue('eprAggregatorId', id)}
+              selectedValue={businessForm.values.eprAggregatorId}
+              containerStyle={{ borderRadius: 10, backgroundColor: Colors.grayTwo, paddingLeft: 10 }}
+            />
+           { clickLogin && !isEmpty(businessForm.values.eprPartnerId)
+            && isEmpty(businessForm.values.eprAggregatorId) ?
+            <CustomText fontSize={15} color={Colors.red} styling={{ marginTop: RfH(10) }}>Select EPR Partner's Aggregator              
+            </CustomText> : null}
+
+          </View>
+
         </View>
       </View>
 
